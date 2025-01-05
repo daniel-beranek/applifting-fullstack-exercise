52cf02d5aaf606ade909df2da5c800e4f1968778
Daniel Beránek <daniel_beranek@icloud.com>
2025-01-05 20:24:54 +0100

    setup axios instance

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,4 +1,16 @@
-export default function ArticlesPage() {
+import { GetStaticProps } from 'next';
+import axiosInstance from '@/lib/features/api/axiosInstance';
+
+type Props = {
+  articles: string;
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const res = await axiosInstance.get('/articles');
+  return { props: { articles: JSON.stringify(res, null, 2) } };
+};
+
+export default function ArticlesPage({ articles }: Props) {
   return (
     <main>
       <h1>Recent articles</h1>
@@ -6,6 +18,7 @@ export default function ArticlesPage() {
         <li>article 1</li>
         <li>article 2</li>
         <li>article 3</li>
+        <li className="mx-auto w-1/2">{articles}</li>
       </ul>
     </main>
   );
