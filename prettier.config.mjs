43a7c8f9a275c80f2c8ce6a7dc2c5dbf752b4ad2
Daniel Beránek <daniel_beranek@icloud.com>
2025-02-08 20:50:02 +0100

    feat: configure prettier
    Signed-off-by: Daniel Beránek <daniel_beranek@icloud.com>

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+'use strict'
+
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: 'es5',
+  semi: false,
+  singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+export default config
