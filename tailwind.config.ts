4068ef03d0765e83ff5c59ebe91f58ecddee63c1
Daniel Beránek <daniel_beranek@icloud.com>
2025-02-25 19:40:36 +0100

    refactor(tailwind): specify content folders
    Signed-off-by: Daniel Beránek <daniel_beranek@icloud.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,8 @@ export default {
   darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/features/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/lib/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
